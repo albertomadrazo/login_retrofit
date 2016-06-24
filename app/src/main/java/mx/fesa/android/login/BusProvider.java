80a3d3b042c3b9e2b9088d6f1d2bd8586f09e7e5
unknown <it04@IT04.FESA.com>
2016-06-24 12:57:14 -0500

    Tutorial de retrofit 1.9 Login con GET y POST.

---
+++ app/src/main/java/mx/fesa/android/login/BusProvider.java
@@ -0,0 +1,14 @@
+package mx.fesa.android.login;
+
+import com.squareup.otto.Bus;
+
+public class BusProvider {
+
+    private static final Bus BUS = new Bus();
+
+    public static Bus getInstance(){
+        return BUS;
+    }
+
+    public BusProvider(){}
+}
---
+++ app/src/main/java/mx/fesa/android/login/Interface.java
@@ -0,0 +1,26 @@
+package mx.fesa.android.login;
+
+import retrofit.Callback;
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
+import retrofit.http.GET;
+import retrofit.http.POST;
+import retrofit.http.Query;
+
+public interface Interface {
+
+    //This method is used for "POST"
+    @FormUrlEncoded
+    @POST("/api.php")
+    void postData(@Field("method") String method,
+                  @Field("username") String username,
+                  @Field("password") String password,
+                  Callback<ServerResponse> serverResponseCallback);
+
+    //This method is used for "GET"
+    @GET("/api.php")
+    void getData(@Query("method") String method,
+                 @Query("username") String username,
+                 @Query("password") String password,
+                 Callback<ServerResponse> serverResponseCallback);
+}
